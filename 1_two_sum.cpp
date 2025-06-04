7c9dcfbb2c89ac67eb591a437a8af2180559bc14
NirshalChandraSekar <nirshalniru@gmail.com>
2025-06-03 20:05:11 -0500

    updated two sum

--- 1_two_sum.cpp
+++ 1_two_sum.cpp
@@ -3,20 +3,20 @@
 using namespace std;
 
 class Solution {
-    public:
-        vector<int> twoSum(vector<int>& nums, int target) {
-            vector<int> output;
-            for(int i=0; i<nums.size()-1; i++){
-                for(int j=i+1; j<nums.size(); j++){
-                    if( (nums[i]+nums[j]) == target){
-                        output.push_back(i);
-                        output.push_back(j);
-                        return output;
-                    }
-                }
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> output;
+        unordered_map<int,int> hashmap;
+        for(int i=0; i<nums.size(); i++){
+            if(hashmap.count(target-nums[i]) >0){
+                return {hashmap[target-nums[i]], i};
             }
-            return output;
-    };
+            else{
+                hashmap[nums[i]] = i;
+            }
+        }
+        return{};
+    }
 };
 
 int main() {
